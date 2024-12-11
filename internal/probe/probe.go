3591812fe46da66f6a86903f67802ad25354ed0e
Mark Rosemaker <48681726+MarkRosemaker@users.noreply.github.com>
2024-12-11 18:23:36 +0100

    regenerate

--- internal/probe/probe.go
+++ internal/probe/probe.go
@@ -6,6 +6,18 @@ const serverURL = "https://api.track.toggl.com/api/v9"
 
 // probe calls the API server to check what we can do
 func probe() error {
-	_, err := http.Get(serverURL + "/me")
+	// In general, for HTTP Basic Auth, you have to add the Authorization header with the request. The Authorization header is constructed as follows:
+
+	// In case email and password are used, they are combined into a email:password format
+	// In case the api token is used, it is combined in xxxx:api_token format (xxxx indicating user's personal token)
+	// The resulting string literal is then encoded using Base64
+	// The authorization method and a space i.e. "Basic " is then put before the encoded string.
+
+	req, err := http.NewRequest(http.MethodGet, serverURL+"/me", nil)
+	if err != nil {
+		return err
+	}
+
+	_, err = http.DefaultClient.Do(req)
 	return err
 }
