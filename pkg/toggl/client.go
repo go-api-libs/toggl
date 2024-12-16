709b54f44888efcac1b03a90f1f9c3e5ac24abac
Mark Rosemaker <48681726+MarkRosemaker@users.noreply.github.com>
2024-12-16 02:44:44 +0100

    add DurationRunning

--- pkg/toggl/client.go
+++ pkg/toggl/client.go
@@ -1,5 +1,10 @@
 package toggl
 
+import "time"
+
+// When creating a new running time entry, use DurationRunning as the duration.
+const DurationRunning = -time.Second
+
 // NewClientWithAPIToken creates a client that authenticates requests using an API token.
 // You can find your API token at the bottom of the [profile page].
 //
