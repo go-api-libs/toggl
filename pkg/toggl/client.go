f7b5de81362f833421dcb0b7b094a4318d601c45
Mark Rosemaker <48681726+MarkRosemaker@users.noreply.github.com>
2026-09-21 04:05:50 +0200

    set token

--- pkg/toggl/client.go
+++ pkg/toggl/client.go
@@ -2,24 +2,30 @@ package toggl
 
 import (
 	"context"
-	"encoding/base64"
+	"os"
 	"time"
 )
 
 // When creating a new running time entry, use DurationRunning as the duration.
 const DurationRunning = -time.Second
 
-// NewClientWithAPIToken creates a client that authenticates requests using an API token.
-// You can find your API token at the bottom of the [profile page].
-//
-// [profile page]: https://track.toggl.com/profile
-func NewClientWithAPIToken(apiToken string, opts ...ClientOption) (*Client, error) {
-	username := apiToken
-	password := "api_token"
-	token := "Basic " + base64.StdEncoding.EncodeToString([]byte(username+":"+password))
-	_ = token
+const (
+	keyToken    = "TOGGL_TOKEN"
+	keyUserName = "TOGGL_API_USERNAME"
+	keyPassword = "TOGGL_API_PASSWORD"
+)
 
-	return NewClient(opts...)
+func init() {
+	// You can find your API token at the bottom of the [profile page].
+	//
+	// [profile page]: https://track.toggl.com/profile
+	token := os.Getenv("TOGGL_TOKEN")
+	if token == "" || os.Getenv(keyUserName) != "" || os.Getenv(keyPassword) != "" {
+		return
+	}
+
+	os.Setenv(keyUserName, token)
+	os.Setenv(keyPassword, "api_token")
 }
 
 func (c *Client) ListTimeEntriesInRange(ctx context.Context, start, end time.Time) (*TimeEntries, error) {
